7a336053de19d6f5cc50a571098ae0196eeed74e
Yugi-Seong <tjddbrl64@gmail.com>
2025-02-25 12:46:45 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { FaAddressBook } from "react-icons/fa";
+function App() {
+
+
+  return (
+    <div className='w-full h-full bg-blue-200'>
+      <div className='w-full flex justify-center items-center p-10'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className='text-4xl italic'>Vite + React</h1>
+      <div className="card">
+
+        <p className='text-orange-400 italic'>
+        <FaAddressBook className='justify-items-center' />
+          부산대학교 KDT 1기 성유기
+          
+        </p>
+      </div>
+      <p className="read-the-docs  hover:bg-sky-700">
+      </p>
+    </div>
+  )
+}
+
+export default App
