3abe4a7764b447d468e1dd2c10a4cf8702501637
Yugi-Seong <tjddbrl64@gmail.com>
2025-03-09 22:50:45 +0900

    foodCard - category

--- src/06/FoodMain.jsx
+++ src/06/FoodMain.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FoodCard from './FoodCard'
 import foodData from"./fooddata.json"
+import TailButton from '../UI/TailButton';
 
 export default function FoodMain() {
   
   console.log(foodData);
   
-  
+  // 1. 카테고리 목록을 중복 제거 후 생성
+  const categories = [...new Set(foodData.map( (item) => item['구분']))] ;
+  console.log(categories);
+
+  // 2. 선택된 카테고리 상태 관리
+  const [selectedCategory, setSelectedCategory] = useState("전체");
+ 
+  // 3. 선택된 카테고리에 맞는 데이터 필터링 
+  const filteredData = 
+  selectedCategory === "전체" ? foodData : foodData.filter((item) =>item["구분"] === selectedCategory);
+
+
   return (
     <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-4'>
-      {foodData.map((data, index) => (
-        <FoodCard key={index} data ={data}/> 
+      {/* {버튼 클릭시 카드 표시} */}
+      <TailButton 
+        caption={"전체"}
+        color="lime"
+        onClick={() => setSelectedCategory("전체")} 
+      />
+
+      {categories.map((category,index) => (
+        <TailButton 
+        key={index}
+        caption={category}
+        color="lime"
+        onClick={() => setSelectedCategory(category)}
+        />       
       ))}
-       
+
+
+      {/* {필터링 된 카드 목록} */}
+      {filteredData.map((data,index) => (
+        <FoodCard key={index} data={data} />
+      ))}
+
     </div>
   )
 }
--- src/UI/TailButton.jsx
+++ src/UI/TailButton.jsx
@@ -4,16 +4,17 @@ export default function TailButton({ caption, color, onClick }) {
   const bg = {
     "blue": "bg-blue-700",
     "orange": "bg-orange-700",
+    "lime": "bg-lime-900",
   }
 
   const bgHover = {
-    "blue": "bg-blue-700",
-    "orange": "bg-orange-700",
-    "lime": "hover:bg-blue-400",
+    "blue": "hover:bg-blue-700",
+    "orange": "hover:bg-orange-700",
+    "lime": "hover:bg-lime-900",
   }
   return (
 
-    <button className={`${bg[color]} ${bgHover[color]} p-2 px-4 text-lg text-white hover:font-bold 
+    <button className={`${bg[color]} ${bgHover[color]}  p-2 px-4 text-lg text-white hover:font-bold 
                         rounded-sm `} onClick={onClick}>
       {caption}
     </button>
